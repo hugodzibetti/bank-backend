316a9aed270b02a810ae2d84cf9b5a18aa7ed304
hugodzibetti <hugodzibetti@gmail.com>
2026-06-01 19:45:55 -0400

    feat(auth): implement JWT-based authentication with guards and strategy
    Add `JwtGuard` and `JwtStrategy` to enable JWT-based authentication across the application. Register `JwtGuard` as a global guard in `UserModule` to protect routes by default. Include `JwtStrategy` in `AuthModule` for token validation. Introduce a constants file for JWT configuration to streamline usage.

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtGuard } from '../auth/guards/jwt.guard';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService],
+  providers: [
+    UserService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtGuard,
+    },
+  ],
 })
 export class UserModule {}
