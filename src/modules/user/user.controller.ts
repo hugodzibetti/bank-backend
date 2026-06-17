3c13290a1edc50b9621766ece3ca0b432c9f395f
hugodzibetti <hugodzibetti@gmail.com>
2026-06-17 16:46:31 -0400

    feat(user): Add endpoints for fetching user profile and details

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CurrentUser } from '../../common/decorators/current-user.decorator';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get('me')
+  getMe(@CurrentUser('id') userId: number) {
+    return this.userService.getMe(userId);
+  }
+
+  @Get(':id')
+  getUserById(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.getUserById(id);
+  }
+}
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { UserProfileDto } from './dto/user-profile.dto';
+import { UserBasicDto } from './dto/user-basic.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {}
+
+  async getMe(userId: number): Promise<UserProfileDto> {
+    const user = await this.usersRepository.findOne({
+      where: { id: userId },
+    });
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    return { id: user.id, name: user.name, email: user.email };
+  }
+
+  async getUserById(id: number): Promise<UserBasicDto> {
+    const user = await this.usersRepository.findOne({
+      where: { id },
+    });
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    return { id: user.id, name: user.name };
+  }
+}
