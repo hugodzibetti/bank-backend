7b48f74ef59b44a4fc423fbbdbcbc17540f52ad4
hugodzibetti <hugodzibetti@gmail.com>
2026-06-18 15:33:29 -0400

    refactor(tests): Split Jest configuration into projects

--- jest.config.js
+++ jest.config.js
@@ -1,27 +1,35 @@
-const fs = require('fs');
-const path = require('path');
-
-const rootDir = '.';
-const absoluteRootDir = fs.realpathSync(rootDir);
-const alternativeRootDir = absoluteRootDir.replace(/^\/var\//, '/');
-
-const roots = [rootDir];
-if (alternativeRootDir !== absoluteRootDir && fs.existsSync(alternativeRootDir)) {
-  roots.push(alternativeRootDir);
-}
-
 module.exports = {
-  globalSetup: '<rootDir>/test/jest-global-setup.ts',
-  globalTeardown: '<rootDir>/test/jest-global-teardown.ts',
-  moduleFileExtensions: ['js', 'json', 'ts'],
-  rootDir: rootDir,
-  roots: roots,
-  testRegex: '.*\\.(spec|e2e-spec)\\.ts$',
-  testPathIgnorePatterns: ['/node_modules/', '/coverage/', '/dist/'],
-  transform: {
-    '^.+\\.(t|j)s$': 'ts-jest',
-  },
-  collectCoverageFrom: ['src/**/*.(t|j)s'],
-  coverageDirectory: './coverage',
-  testEnvironment: 'node',
+  projects: [
+    {
+      displayName: 'unit',
+      moduleFileExtensions: ['js', 'json', 'ts'],
+      rootDir: '.',
+      testMatch: ['<rootDir>/**/*.spec.ts'],
+      testPathIgnorePatterns: [
+        '/node_modules/',
+        '/coverage/',
+        '/dist/',
+        '/test/',
+      ],
+      transform: {
+        '^.+\\.(t|j)s$': 'ts-jest',
+      },
+      collectCoverageFrom: ['src/**/*.(t|j)s'],
+      coverageDirectory: './coverage',
+      testEnvironment: 'node',
+    },
+    {
+      displayName: 'e2e',
+      moduleFileExtensions: ['js', 'json', 'ts'],
+      rootDir: '.',
+      testMatch: ['<rootDir>/test/e2e/**/*.e2e-spec.ts'],
+      transform: {
+        '^.+\\.(t|j)s$': 'ts-jest',
+      },
+      testEnvironment: 'node',
+      globalSetup: './test/jest-global-setup.ts',
+      globalTeardown: './test/jest-global-teardown.ts',
+      testTimeout: 30000,
+    },
+  ],
 };
